44d8660282584a4df330e76dd2a2dfc19ffd9111
Brian Lewis <blewis@lewisinsurance.com>
2026-08-24 16:09:11 -0400

    Fix trust claims and add verified Google reviews (batch 2/5)

---
+++ src/data/googleReviews.ts
@@ -0,0 +1,89 @@
+export type GoogleReview = {
+    name: string
+    when: string
+    text: string
+}
+
+export const googleReviews: GoogleReview[] = [
+    {
+        name: "Teresa Mack-Anderson",
+        when: "Edited 6 years ago",
+        text: "I would like to thank Mrs. Kelli Lee for the excellent service she has provided for me and my family. She goes above and beyond her duties to make sure that any questions that we may have are answered in a timely manner. She is very knowledgeable and ensures that we are given the best possible rates on any insurance policy. She is very courteous and is always willing to help. I have recommended Mrs. Kelli Lee to many of my friends who were in need of an insurance policy. She is by far one of the best when it comes to customer service. Thank you Mrs. Kelli Lee for going that extra mile and you are appreciated.",
+    },
+    {
+        name: "Marvin Deyampert",
+        when: "7 years ago",
+        text: "Lance is extremely courteous, prompt and efficient in assisting me with my insurance policy.Lance was always able to answer any questions I had. He has excellent customer service , professional and knowledgeable. Receptionist is always courteous and friendly. I recommend Mr. Lance.to anyone . He is fantastic To myself and my family he is a Blessing. Thank you very very much to you for your help and the peace mind you have provided. Gratefully yours Marvin and Family",
+    },
+    {
+        name: "shawn turberville",
+        when: "7 years ago",
+        text: "Kelli Lee goes above and beyond to help our family with all our insurance needs and is always willing to help no matter what time of day. With 3 children ranging from 16 to 26 it seems we are always needing something and she gets us answers very quickly. Excellent customer service and I would recommend to anyone !!!!",
+    },
+    {
+        name: "Jeanna Norman",
+        when: "5 years ago",
+        text: "The customer service experience I had with Lewis Insurance Agency was second to none! I recently needed to transfer an existing homeowners policy and I'm so happy with the decision I made to move it here. Lance couldn't have made it any simpler for me! I was able to sign my documents digitally just MOMENTS after requesting the transfer. I think it's important to note that I contacted this agency around 4PM and their office closes at 5. My request could have been postponed until the following business day, but it wasn't. In fact, within the hour he was on the phone with underwriting to put a rush on transferring the policy because we were on a pretty tight timeline. By 5:00 he informed me that the insurance company had processed the change. The next morning I received a message from him that he had received the renewal from the company and would be forwarding it to our lender. The turn around time was simply incredible and I can't imagine a better customer service experience than the one I had at Lewis Insurance Agency! Looking forward to being a customer for many years to come.",
+    },
+    {
+        name: "Joe Barbera",
+        when: "5 years ago",
+        text: "Lance was a wonderful representative to deal with! Looked over our previous insurance and found us the same and better coverage for much less than we were paying after being with the prior company for over 20 years. He also keeps in touch and continues to help find the best deals with the same coverage!",
+    },
+    {
+        name: "Kelly Bevington",
+        when: "3 years ago",
+        text: "I have used Lewis and Lewis for more than 15 years that I can count... and I have always had reliable helpful service. Lance is spectacular!! Thank you Lance for always taking excellent care of my family and my business needs!!",
+    },
+    {
+        name: 'Michael "Mike Montgomery" Montgomery',
+        when: "4 years ago",
+        text: "I always get great service for all my insurance needs when I work with the folks at Lewis Insurance. They understand their business are the insurance experts and have help advise us on our insurance needs. You can trust what they tell you and they work hard to find you the best rates available for your insurance needs.\n\nGive them a call to discuss how they can help you with your insurance.",
+    },
+    {
+        name: "Windi Garret",
+        when: "Edited 6 years ago",
+        text: "Kelli Lee is amazing. She makes you feel like you are her best friend and only customer. She gets things done as quickly as possible and always make sure you get the best deal. I would recommend Kelli and Lewis and Lewis to everyone in the area!!",
+    },
+    {
+        name: "David Kolp",
+        when: "4 years ago",
+        text: "I have dealt with many different insurance companies, and Lance McDonald at Lewis Insurance Agency was the best. Lance always got back with me within a couple hours, and always obtained the information I needed. He worked very hard to obtain me the best insurance at the best price. I highly recommend Lance and the Lewis Insurance Agency.",
+    },
+    {
+        name: "Jason Blair",
+        when: "6 years ago",
+        text: "Lance always goes out of his way to answer all my questions and provide me with the best quality home and auto coverage available on the market. I am a customer for life and highly recommend him and Lewis Insurance for all your insurance needs.",
+    },
+    {
+        name: "James Solomon",
+        when: "5 years ago",
+        text: "I have had Insurance with one of the largest auto insurance companies that say they save you 15% They constantly raised my rates, as a reward for being a loyal customer over 15 years. I noticed Lewis & Lewis Ins does business, and shops your ins rate with many companies. So, I came in the office for a quote, and left with the same Ins coverage with another company, and saved just over 50%\n\nThanks Kelli.",
+    },
+    {
+        name: "Stephanie Watson",
+        when: "5 years ago",
+        text: "Lance is honestly the reason I use this firm for my auto and boat insurance. He is very responsive, honest and overall a really great guy. He was recommended to me by a friend and I am always happy to recommended him as well.",
+    },
+    {
+        name: "Josh Anderson",
+        when: "5 years ago",
+        text: "Lance was great to work with. He helped me get an insurance policy for my home and also helped me to save money compared to previous policies carried. Overall a great experience, I am very pleased.",
+    },
+    {
+        name: "stephanee macdonald",
+        when: "7 years ago",
+        text: "Outstanding Customer Service! The staff at Lewis Insurance goes above and beyond to find the right policy for you and your family's needs. Our family was able to save quite a bit on our insurance premiums and was able to increase our protection when we moved to Lewis Insurance. At Lewis Insurance, you will find service with a smile and will be treated with respect.",
+    },
+    {
+        name: "Kathy Dice",
+        when: "5 years ago",
+        text: "Thank you to KELLI at Lewis Insurance Agency for her professional and expedient response to my insurance needs. Have been with The Lewis Insurance Agency in Lake City for 25 years and their prompt attention to their customers keeps me coming back!",
+    },
+]
+
+export const homeGoogleReviews = [
+    googleReviews[3],
+    googleReviews[5],
+    googleReviews[14],
+]
